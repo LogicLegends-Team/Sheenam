cf109b789b89151d9b1bee84cedd5b75dc2e4239
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2024-01-24 12:36:23 +0500

    BROKERS: Insert User

---
+++ Sheenam/Brokers/Storages/IStorageBroker.User.cs
@@ -0,0 +1,9 @@
+﻿using Sheenam.Models.Foundations.Users;
+
+namespace Sheenam.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<User> InsertUserAsync(User user);
+    }
+}
