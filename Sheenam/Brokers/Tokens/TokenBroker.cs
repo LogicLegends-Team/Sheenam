4730d56a334eb2c465008d17fdeb1ef2676d1a14
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2024-01-29 16:14:21 +0500

    BROKERS: Generate JWT Token

---
+++ Sheenam/Brokers/Tokens/TokenBroker.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Sheenam.Models.Foundations.Tokens;
+using Sheenam.Models.Foundations.Users;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Sheenam.Brokers.Tokens
+{
+    public class TokenBroker : ITokenBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public UserToken GenerateJWTToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Role,user.Role.ToString()),
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("Jwt:Key").Value));
+
+            var signingCred = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+
+            var securityToken = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: signingCred
+            );
+
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+
+            return new UserToken
+            {
+                Token = tokenString,
+            };
+        }
+    }
+}
---
+++ Sheenam/Models/Foundations/Tokens/UserToken.cs
@@ -0,0 +1,7 @@
+﻿namespace Sheenam.Models.Foundations.Tokens
+{
+    public class UserToken
+    {
+        public string Token { get; set; }
+    }
+}
